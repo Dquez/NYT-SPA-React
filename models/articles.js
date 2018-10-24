0b8208291aaf8be188cec439b0faf65696146210
Dariell Vasquez <dariellv@yahoo.com>
2018-10-24 12:38:40 -0400

    fixed 422 error by removing require byline parameter

--- models/articles.js
+++ models/articles.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const ArticleSchema = new Schema({
   _id: { type: Schema.ObjectId},
   headline: { type: String, required: true },
-  byline: { type: String, required: true },
+  byline: { type: String },
   web_url: { type: String, required: true},
   date: { type: Date, default: Date.now },
   isSaved: {type: Boolean, default: false}
